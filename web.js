7c9646da541096ab699a06258cc91658adfef01a
Ubuntu <ubuntu@ip-172-31-37-49.us-west-2.compute.internal>
2013-07-11 07:10:11 +0000

    read file sync was not called correctly..

--- web.js
+++ web.js
@@ -4,11 +4,11 @@ var fs = require('fs');
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
-  fs.readFileSync('index.html', function(err, data){
-      var buf = new Buffer(data, 'utf8');
-      response.send(buf.toString);
-  });  
+
+  var buf = new Buffer( fs.readFileSync('index.html'), 'utf8');
+  response.send(buf.toString);
 });
+
 var port = process.env.PORT || 5000;
 app.listen(port, function() {
   console.log("Listening on " + port);
