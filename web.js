dac2cf83c24285dfdb36bfdf2b07b66feb466ec6
Ubuntu <ubuntu@ip-172-31-37-49.us-west-2.compute.internal>
2013-07-11 07:11:51 +0000

    bufer toString is a method..

--- web.js
+++ web.js
@@ -6,7 +6,7 @@ var app = express.createServer(express.logger());
 app.get('/', function(request, response) {
 
   var buf = new Buffer( fs.readFileSync('index.html'), 'utf8');
-  response.send(buf.toString);
+  response.send(buf.toString());
 });
 
 var port = process.env.PORT || 5000;
