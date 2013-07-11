93819d03ebe6c3fa6364f303829ee09f70fd954a
Ubuntu <ubuntu@ip-172-31-37-49.us-west-2.compute.internal>
2013-07-11 06:57:04 +0000

    Fixed web.js syntax error

--- web.js
+++ web.js
@@ -7,7 +7,7 @@ app.get('/', function(request, response) {
   fs.readFileSync('index.html', function(err, data){
       response.send(data.toString);
   });  
-
+});
 var port = process.env.PORT || 5000;
 app.listen(port, function() {
   console.log("Listening on " + port);
